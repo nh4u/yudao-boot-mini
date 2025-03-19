f327682bb7af679246e314559e88b4865a41e172
崔家赟 <707100325@qq.com>
2025-03-19 20:40:54 +0800

    解决Bug 1708 1709

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/additionalrecording/AdditionalRecordingMapper.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/additionalrecording/AdditionalRecordingMapper.java
@@ -37,8 +37,8 @@ public interface AdditionalRecordingMapper extends BaseMapperX<AdditionalRecordi
 
     boolean existsByVoucherId(@Param("voucherId") Long voucherId);
 
-    @Select("SELECT DISTINCT voucher_id FROM ems_additional_recording " +
-            "WHERE standingbook_id = #{standingbookId} AND deleted = 0")
+    @Select("SELECT voucher_id FROM ems_additional_recording " +
+            "WHERE standingbook_id = #{standingbookId} AND deleted = 0 AND voucher_id IS NOT NULL")
     List<Long> selectVoucherIdsByStandingbookId(@Param("standingbookId") Long standingbookId);
 
     @Select({
