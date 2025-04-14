6915a2321331f725728bea3ff74395b0c5609706
nh4u <2496905587@qq.com>
2025-04-14 18:40:20 +0800

    :poop: 添加数采模块

---
+++ ems-module-acquisition/ems-module-acquisition-api/src/main/java/cn/bitlinks/ems/module/acquisition/enums/ErrorCodeConstants.java
@@ -0,0 +1,14 @@
+package cn.bitlinks.ems.module.acquisition.enums;
+
+import cn.bitlinks.ems.framework.common.exception.ErrorCode;
+
+/**
+ * acquisition  错误码枚举类
+ *
+ * acquisition 系统，使用 1-001-000-000 段
+ */
+public interface ErrorCodeConstants {
+
+
+
+}
---
+++ ems-module-acquisition/ems-module-acquisition-biz/src/main/java/cn/bitlinks/ems/module/acquisition/AcquisitionServerApplication.java
@@ -0,0 +1,39 @@
+package cn.bitlinks.ems.module.acquisition;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.TimeZone;
+
+/**
+ * 项目的启动类
+ *
+ * @author bitlinks
+ */
+@Slf4j
+@SpringBootApplication
+public class AcquisitionServerApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+        ConfigurableApplicationContext application = SpringApplication.run(AcquisitionServerApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "应用 '{}' 启动成功! 访问连接:\n\t" +
+                        "Swagger文档: \t\thttp://{}:{}/doc.html\n\t" +
+                        "数据库监控: \t\thttp://{}:{}/druid\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port", "8080"),
+                "127.0.0.1",
+                env.getProperty("server.port", "8080"));
+    }
+
+}
