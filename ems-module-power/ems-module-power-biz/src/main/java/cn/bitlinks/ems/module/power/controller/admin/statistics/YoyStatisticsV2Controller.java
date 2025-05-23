d95b66358e38e7d5a647f46a3d81390631de3a3f
lizhenfu <lizhenfu@163.com>
2025-05-23 14:08:33 +0800

    :sparkles: 用能分析——同比分析

---
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/controller/admin/statistics/YoyStatisticsV2Controller.java
@@ -0,0 +1,58 @@
+package cn.bitlinks.ems.module.power.controller.admin.statistics;
+
+import cn.bitlinks.ems.framework.common.pojo.CommonResult;
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.ComparisonChartResultVO;
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsParamV2VO;
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsResultV2VO;
+import cn.bitlinks.ems.module.power.service.statistics.ComparisonV2Service;
+import cn.bitlinks.ems.module.power.service.statistics.YoyV2Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import static cn.bitlinks.ems.framework.common.pojo.CommonResult.success;
+
+
+@Tag(name = "管理后台 - 用能分析/同比分析")
+@RestController
+@RequestMapping("/power/statistics/yoy/v2")
+@Validated
+public class YoyStatisticsV2Controller {
+
+    @Resource
+    private YoyV2Service yoyV2Service;
+
+
+    @PostMapping("/discountAnalysisTable")
+    @Operation(summary = "折价同比分析（表）")
+    public CommonResult<StatisticsResultV2VO> discountAnalysisTable(@Valid @RequestBody StatisticsParamV2VO paramVO) {
+        return success(yoyV2Service.discountAnalysisTable(paramVO));
+    }
+
+    @PostMapping("/discountAnalysisChart")
+    @Operation(summary = "折价同比分析（图）")
+    public CommonResult<ComparisonChartResultVO> discountAnalysisChart(@Valid @RequestBody StatisticsParamV2VO paramVO) {
+        return success(yoyV2Service.discountAnalysisChart(paramVO));
+
+    }
+
+    @PostMapping("/foldCoalAnalysisTable")
+    @Operation(summary = "折煤同比分析（表）")
+    public CommonResult<StatisticsResultV2VO> foldCoalAnalysisTable(@Valid @RequestBody StatisticsParamV2VO paramVO) {
+        return success(yoyV2Service.foldCoalAnalysisTable(paramVO));
+    }
+
+    @PostMapping("/foldCoalAnalysisChart")
+    @Operation(summary = "折煤同比分析（图）")
+    public CommonResult<ComparisonChartResultVO> foldCoalAnalysisChart(@Valid @RequestBody StatisticsParamV2VO paramVO) {
+        return success(yoyV2Service.foldCoalAnalysisChart(paramVO));
+
+    }
+}
