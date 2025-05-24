bbcbcb25a9186a8681caa986eda648107bb1c567
lizhenfu <lizhenfu@163.com>
2025-05-24 11:29:33 +0800

    :sparkles: 统计总览接口

---
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/service/statistics/StatisticsHomeService.java
@@ -0,0 +1,19 @@
+package cn.bitlinks.ems.module.power.service.statistics;
+
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsHomeResultVO;
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsParamV2VO;
+import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsParamVO;
+
+/**
+ * 统计总览 Service 接口
+ */
+public interface StatisticsHomeService {
+
+    /**
+     * 统计总览接口
+     *
+     * @param paramVO
+     * @return
+     */
+    StatisticsHomeResultVO overview(StatisticsParamV2VO paramVO);
+}
