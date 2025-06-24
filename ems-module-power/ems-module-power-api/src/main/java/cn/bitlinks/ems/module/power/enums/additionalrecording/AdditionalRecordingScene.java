3694244a3dd16db7027c8f0fc61fb281e0e5665d
nh4u <2496905587@qq.com>
2025-06-24 18:02:12 +0800

    :poop: 2125 【数据补录】当所选时间为业务数据点位时，补录后时间线展示错误。

--- ems-module-power/ems-module-power-api/src/main/java/cn/bitlinks/ems/module/power/enums/additionalrecording/AdditionalRecordingScene.java
+++ ems-module-power/ems-module-power-api/src/main/java/cn/bitlinks/ems/module/power/enums/additionalrecording/AdditionalRecordingScene.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 public enum AdditionalRecordingScene {
 
     NO_HIS(1, "无历史数据"),
+    NO_HIS_COVER(11, "无历史数据(当前覆盖)"),
     ONE_NEXT(2, "下一个业务点"),
     ONE_PRE(3, "上一个业务点"),
     TWO_POINT(4, "两个业务点"),
-//    TWO_PRE_COVER(5, "两个业务点与前一个业务点重合"),
-//    TWO_NEXT_COVER(6, "两个业务点与后一个业务点重合"),
+    TWO_POINT_COVER(41, "两个业务点(当前覆盖)"),
+    TWO_PRE_COVER(5, "两个业务点与前一个业务点重合"),
+    TWO_NEXT_COVER(6, "两个业务点与后一个业务点重合"),
     ;
 
     private final Integer code;
