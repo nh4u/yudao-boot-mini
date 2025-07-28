1e2cd9d6f7840275846f9451e5a4917b7bce0566
nh4u <2496905587@qq.com>
2025-07-28 10:19:24 +0800

    :poop: split executor

--- ems-module-acquisition/ems-module-acquisition-api/src/main/java/cn/bitlinks/ems/module/acquisition/api/collectrawdata/dto/MinuteAggregateDataDTO.java
+++ ems-module-acquisition/ems-module-acquisition-api/src/main/java/cn/bitlinks/ems/module/acquisition/api/collectrawdata/dto/MinuteAggregateDataDTO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -17,6 +18,7 @@ public class MinuteAggregateDataDTO implements Serializable {
     /**
      * 聚合时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime aggregateTime;
     /**
      * 参数 code
