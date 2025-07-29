32334a191da1ca2c15db066c98ac37dfd14d8dba
liumingqiang <654751637@qq.com>
2025-07-29 11:57:01 +0800

    :sparkles: 折标煤用量定基比、用能成本定基比导出

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/service/statistics/BaseV2Service.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/service/statistics/BaseV2Service.java
@@ -2,8 +2,9 @@ package cn.bitlinks.ems.module.power.service.statistics;
 
 import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.BaseStatisticsParamV2VO;
 import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.ComparisonChartResultVO;
-import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsParamV2VO;
 import cn.bitlinks.ems.module.power.controller.admin.statistics.vo.StatisticsResultV2VO;
+
+import java.util.List;
 
 /**
  * 用能分析 定基比分析 Service 接口
@@ -39,4 +40,8 @@ public interface BaseV2Service {
      * @return
      */
     ComparisonChartResultVO foldCoalAnalysisChart(BaseStatisticsParamV2VO paramVO);
+
+    List<List<String>> getExcelHeader(BaseStatisticsParamV2VO paramVO, Integer flag);
+
+    List<List<Object>> getExcelData(BaseStatisticsParamV2VO paramVO, Integer flag);
 }
