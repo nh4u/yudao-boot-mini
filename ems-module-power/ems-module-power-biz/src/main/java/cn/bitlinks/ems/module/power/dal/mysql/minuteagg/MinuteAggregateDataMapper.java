69bd86344c76975ffb1ec72f8547ab5f035867df
liumingqiang <654751637@qq.com>
2025-08-08 09:57:16 +0800

    :sparkles: 供水温度取值接口

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/minuteagg/MinuteAggregateDataMapper.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/minuteagg/MinuteAggregateDataMapper.java
@@ -1,6 +1,12 @@
 package cn.bitlinks.ems.module.power.dal.mysql.minuteagg;
 
+import cn.bitlinks.ems.framework.tenant.core.aop.TenantIgnore;
+import cn.bitlinks.ems.module.power.dal.dataobject.minuteagg.MinuteAggregateDataDO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 分钟聚合数据mapper
@@ -8,6 +14,19 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MinuteAggregateDataMapper {
 
-    // todo 查询稳态值小时级别数据、用量的累积数据
+    /**
+     * 根据台账和能源参数code获取温度聚合数据
+     *
+     * @param standingbookIds
+     * @param paramCodes
+     * @param starTime
+     * @param endTime
+     * @return
+     */
+    @TenantIgnore
+    List<MinuteAggregateDataDO> getTmpRangeDataSteady(@Param("standingbookIds") List<Long> standingbookIds,
+                                                      @Param("paramCodes") List<String> paramCodes,
+                                                      @Param("starTime") LocalDateTime starTime,
+                                                      @Param("endTime") LocalDateTime endTime);
 }
 
