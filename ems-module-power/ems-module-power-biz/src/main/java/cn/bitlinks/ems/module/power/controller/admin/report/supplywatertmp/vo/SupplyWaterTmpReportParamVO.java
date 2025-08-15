942c20397b7a65dd023f06a8bb3575154185d0e0
liumingqiang <654751637@qq.com>
2025-08-15 10:24:16 +0800

    :sparkles: 供水温度表优化

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/controller/admin/report/supplywatertmp/vo/SupplyWaterTmpReportParamVO.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/controller/admin/report/supplywatertmp/vo/SupplyWaterTmpReportParamVO.java
@@ -28,11 +28,6 @@ public class SupplyWaterTmpReportParamVO {
     @Size(min = 2, max = 2, message = "统计周期不能为空")
     private LocalDateTime[] range;
 
-    @Schema(description = "供水图统计周期,最长不超1年", example = "[\"2025-06-23 00:00:00\", \"2025-06-29 00:00:00\" ]")
-    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
-    @Size(min = 2, max = 2, message = "统计周期不能为空")
-    private LocalDateTime[] timeRange;
-
     @Schema(description = "系统")
     private List<String> system;
 
@@ -46,8 +41,5 @@ public class SupplyWaterTmpReportParamVO {
     @Schema(description = "班组标记 0：非班组；1：班组。")
     @NotNull(message = "班组不能为空")
     private Integer teamFlag;
-
-    @Schema(description = "单个系统")
-    private String singleSystem;
 
 }
