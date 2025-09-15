8f58eaa7ac958477cf30c6a69b06714db1ac1d2d
liumingqiang <654751637@qq.com>
2025-09-15 16:47:25 +0800

    :bug: 数据导出接口 搜索bug

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/doublecarbon/DoubleCarbonMappingMapper.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/dal/mysql/doublecarbon/DoubleCarbonMappingMapper.java
@@ -2,18 +2,20 @@ package cn.bitlinks.ems.module.power.dal.mysql.doublecarbon;
 
 import cn.bitlinks.ems.framework.common.pojo.PageResult;
 import cn.bitlinks.ems.framework.mybatis.core.mapper.BaseMapperX;
-import cn.bitlinks.ems.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.bitlinks.ems.module.power.controller.admin.doublecarbon.vo.DoubleCarbonMappingPageReqVO;
 import cn.bitlinks.ems.module.power.dal.dataobject.doublecarbon.DoubleCarbonMappingDO;
+import cn.hutool.core.text.CharSequenceUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.apache.ibatis.annotations.Mapper;
 
 
 @Mapper
 public interface DoubleCarbonMappingMapper extends BaseMapperX<DoubleCarbonMappingDO> {
     default PageResult<DoubleCarbonMappingDO> selectPage(DoubleCarbonMappingPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<DoubleCarbonMappingDO>()
-                .likeIfPresent(DoubleCarbonMappingDO::getDoubleCarbonCode, reqVO.getStandingbookCode())
-                .likeIfPresent(DoubleCarbonMappingDO::getStandingbookCode, reqVO.getStandingbookCode()));
+        return selectPage(reqVO, new LambdaQueryWrapper<DoubleCarbonMappingDO>()
+                .like(CharSequenceUtil.isNotBlank(reqVO.getStandingbookCode()), DoubleCarbonMappingDO::getStandingbookCode, reqVO.getStandingbookCode())
+                .or()
+                .like(CharSequenceUtil.isNotBlank(reqVO.getStandingbookCode()), DoubleCarbonMappingDO::getDoubleCarbonCode, reqVO.getStandingbookCode()));
     }
 
 }
