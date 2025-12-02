ba0cab19c448b5191a0754aa0e9387988989d28e
lizhenfu <1035174590@qq.com>
2025-12-02 14:52:47 +0800

    :poop:接口联调

--- ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/controller/admin/invoice/vo/InvoicePowerRecordPageReqVO.java
+++ ems-module-power/ems-module-power-biz/src/main/java/cn/bitlinks/ems/module/power/controller/admin/invoice/vo/InvoicePowerRecordPageReqVO.java
@@ -1,14 +1,13 @@
 package cn.bitlinks.ems.module.power.controller.admin.invoice.vo;
 
-import cn.bitlinks.ems.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDate;
 
-@Schema(description = "管理后台 - 发票电量记录分页查询 Request VO")
+@Schema(description = "管理后台 - 发票电量记录列表查询 Request VO")
 @Data
-public class InvoicePowerRecordPageReqVO extends PageParam {
+public class InvoicePowerRecordPageReqVO {
 
     @Schema(description = "补录月份区间 [开始, 结束]", example = "[2025-09-01, 2025-12-01]")
     private LocalDate[] recordMonth;
